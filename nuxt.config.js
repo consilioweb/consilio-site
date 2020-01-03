2eca548528734b5b5dc0b2836a9ed9b47228a150
Humberto Martins <humberto@consilio.com.br>
2020-01-03 08:38:43 -0300

    Add CORS headers axios

--- nuxt.config.js
+++ nuxt.config.js
@@ -184,8 +184,9 @@ export default {
     proxy: false,
     headers: {
       "Access-Control-Allow-Origin": "*",
-      "Access-Control-Allow-Methods": "GET, POST, OPTIONS",
-      "Access-Control-Allow-Headers": "Cache-Control,Content-Type,Authorization"
+      "Access-Control-Allow-Methods": "GET,HEAD,OPTIONS,POST,PUT",
+      "Access-Control-Allow-Headers":
+        "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers"
     },
     /*
     proxyHeaders: true,
