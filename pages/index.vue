6451f586b0bce4b5d5d67687e44aa1f7823db98b
Humberto Martins <humberto@consilio.com.br>
2020-01-07 09:32:41 -0300

    Lazy correction

--- pages/index.vue
+++ pages/index.vue
@@ -4,35 +4,19 @@
     <about :about="about" />
     <methodology />
     <cases :cases="cases" :length="lengthCases" />
-    <v-lazy when-visible>
-      <recent-posts :posts="recent" :length="lengthPosts" />
-    </v-lazy>
+    <recent-posts :posts="recent" :length="lengthPosts" />
     <testimonials :testimonials="testimonials" :length="lengthTestimonials" />
     <clients :clients="clients" :length="lengthClients" />
-    <v-lazy when-visible>
-      <cta
-        title="Marketing Digital inteligente e feito por quem respira comunicação digital."
-        textButton="DIAGNÓSTICO GRATUITO"
-      />
-    </v-lazy>
-    <v-lazy when-visible>
-      <newsletter />
-    </v-lazy>
+    <cta
+      title="Marketing Digital inteligente e feito por quem respira comunicação digital."
+      textButton="DIAGNÓSTICO GRATUITO"
+    />
+    <newsletter />
   </div>
 </template>
 
 <script>
 import { mapState, mapGetters } from "vuex";
-import VLazy from "vue-lazy-hydration";
-//import slides from "@/components/home/slides.vue";
-//import about from "@/components/home/about.vue";
-//import cases from "@/components/home/cases.vue";
-//import methodology from "@/components/home/methodology.vue";
-//import recentPosts from "@/components/blog/recentPosts.vue";
-//import testimonials from "@/components/home/testimonials.vue";
-//import clients from "@/components/home/clients.vue";
-//import cta from "@/components/home/cta.vue";
-//import newsletter from "@/components/home/newsletter.vue";
 
 export default {
   mounted() {
@@ -40,7 +24,6 @@ export default {
     this.$store.commit("HOVER_BUTTON_HEADER", false);
   },
   components: {
-    VLazy,
     slides: () => import("@/components/home/slides.vue"),
     about: () => import("@/components/home/about.vue"),
     methodology: () => import("@/components/home/methodology.vue"),
