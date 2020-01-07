3c0990ab641dff72232b02223eb97407601a1a6e
Humberto Martins <humberto@consilio.com.br>
2020-01-07 15:38:25 -0300

    Init OneSignal

--- layouts/default.vue
+++ layouts/default.vue
@@ -41,6 +41,17 @@ export default {
       lengthMainMenu: "menus/lengthMainMenu"
     })
   },
+  mounted() {
+    this.$OneSignal.push(() => {
+      this.$OneSignal.isPushNotificationsEnabled(isEnabled => {
+        if (isEnabled) {
+          console.log("Push notifications are enabled!");
+        } else {
+          console.log("Push notifications are not enabled yet.");
+        }
+      });
+    });
+  }
 };
 </script>
 
